6bf0db652498a3b7db127b7db9e9f6f1d03d268f
Uros Gluscevic <U.Gluscevic@student.tudelft.nl>
2026-08-04 15:02:33 +0200

    all changes dump

--- debugging/debug.jl
+++ debugging/debug.jl
@@ -2,16 +2,16 @@ using TulipaEnergyModel
 using DuckDB
 using TulipaIO
 
-in_folder = joinpath(pwd(), "test/inputs")
+in_folder = joinpath(pwd(), "debugging/experiment-inputs/regret-reuse")
 # in_folder = joinpath(pwd(), "debugging/experiment-inputs/single-country")
 # bench_folder = joinpath(pwd(), "debugging")
 
 # in_dir = joinpath(in_folder, "Multi-year Investments")
-in_dir = joinpath(in_folder, "1var-0")
+in_dir = joinpath(in_folder, "investment_problem")
 # in_dir = joinpath(in_folder, "all-susd")
 # in_dir = joinpath(in_folder, "UC-ramping")
 # in_dir = joinpath(in_folder, "case-study")
-in_dir = joinpath(in_folder, "complex-case-study")
+# in_dir = joinpath(in_folder, "complex-case-study")
 # in_dir = joinpath(bench_folder, "EU")
 
 # Conversion
